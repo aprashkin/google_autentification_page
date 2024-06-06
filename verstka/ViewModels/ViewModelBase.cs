9bfe6a972e69a75eadb2e99b210117bd781f9b85
MaksimK30 <makskuzmih@gmail.com>
2024-06-06 15:53:52 +0300

    version 1.0

---
+++ verstka/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace verstka.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ verstka/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace verstka.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
